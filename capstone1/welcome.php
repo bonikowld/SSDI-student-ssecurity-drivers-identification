1fb82e1ce7a5f8fcf065a4df50f643ff6aca9a63
bonikowld <bonikowld@users.noreply.github.com>
2023-04-13 12:51:16 +0800

    Update welcome.php
    First Commit
    
    Changes are
    - Fix error in homepage for student account

--- capstone1/welcome.php
+++ capstone1/welcome.php
@@ -1,18 +1,24 @@
-<?php 
+<?php
 
 session_start();
 
 if (!isset($_SESSION['username'])) {
     header("Location: index.php");
 }
-
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome<?php
+<title>Welcome</title>
+</head>
+<body>
+
+<h1>WELCOME!!!!</h1>
+<p>HOTDOG SI MARTY BAHO IGOT2 HAHAHAHAH</p>
+
+</body>
+</html>
+
 
 
